20c1b5f0a5a84ce76c1fb996550b46f632cf7d11
Lockegogo <lmzhao31@outlook.com>
2022-02-17 08:29:15 +0800

    update

--- 贪心算法/摆动序列.py
+++ 贪心算法/摆动序列.py
@@ -1,12 +1,12 @@
 class Solution:
     def wiggleMaxLength(self, nums: List[int]) -> int:
         # 题目里nums长度大于等于1，当长度为1时，其实到不了for循环里去，所以不用考虑nums长度
-        preC, curC, res = 0,0,1  
+        preC, curC, res = 0, 0, 1
         for i in range(len(nums) - 1):
             curC = nums[i + 1] - nums[i]
             # 差值为0时，不算摆动
-            if curC * preC <= 0 and curC !=0:  
+            if curC * preC <= 0 and curC != 0:
                 res += 1
                 # 如果当前差值和上一个差值为一正一负时，才需要用当前差值替代上一个差值
-                preC = curC  
+                preC = curC
         return res
