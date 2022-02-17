5f2fbe2d5e3d2252bfff141d6bb88f774f531263
Lockegogo <lmzhao31@outlook.com>
2022-02-17 08:08:05 +0800

    动态规划

---
+++ 动态规划/爬楼梯.py
@@ -0,0 +1,27 @@
+"""
+假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+
+每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+
+输入：n = 2
+输出：2
+解释：有两种方法可以爬到楼顶。
+1. 1 阶 + 1 阶
+2. 2 阶
+"""
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        # dp[i] 数组存储爬到第 i 层楼梯时有 dp[i] 种方法
+        if n <= 1:
+            return n
+        dp = [0] * (n+1)
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3,n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[-1]
+
+n = 5
+sol = Solution()
+print(sol.climbStairs(n))
